0265fd1e37089f783387e15269ffbfa46030f136
mathmauney <mathmauney@gmail.com>
2019-04-08 16:36:14 -0500

    Anonymize the commands to make it runable if the pump isn't present

--- HardvardControl0.py
+++ HardvardControl0.py
@@ -15,13 +15,13 @@ top = tk.Tk()
 s = tk.Spinbox(top, from_=1, to=10)
 s.pack()
 
-Bset = tk.Button(top, text="Set", command=firstpump.setport(s.get()))
+Bset = tk.Button(top, text="Set", command=lambda: firstpump.setport(s.get()))
 Bset.pack()
 
-Brun = tk.Button(top, text="RUN", command=firstpump.startpump())
+Brun = tk.Button(top, text="RUN", command=firstpump.startpump)
 Brun.pack()
 
-Bstop = tk.Button(top, text="STOP", command=firstpump.stoppump())
+Bstop = tk.Button(top, text="STOP", command=firstpump.stoppump)
 Bstop.pack()
 
 top.mainloop()
