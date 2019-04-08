df703811ac0fcdde0818e76850ff048499865eb5
Derrick <drl222@cornell.edu>
2019-04-08 17:55:58 -0400

    Initial history-saving is working

--- gui.py
+++ gui.py
@@ -5,10 +5,12 @@ from tkinter import filedialog
 from widgets import FluidLevel
 import tkinter.ttk as ttk
 from configparser import SafeConfigParser
-
+import csv
+import time
 
 class main:
     """Class for the main window of the SAXS Control."""
+    CSV_HEADERS = ["Unix time (s)", "oil level (%)"]
 
     def __init__(self, window):
         """Set up the window and button variables."""
@@ -21,6 +23,7 @@ class main:
         self.stop_button = tk.Button(self.buttons, text='Stop', command=self.stop)
         self.save_config_button = tk.Button(self.buttons, text='Save Config', command=self.save_config)
         self.load_config_button = tk.Button(self.buttons, text='Load Config', command=self.load_config)
+        self.save_button = tk.Button(self.buttons, text='Save History', command=self.save_history)
         # Main Structures
         self.core = ttk.Notebook(self.main_window)
         self.main_page = tk.Frame(self.core)
@@ -32,6 +35,14 @@ class main:
         self.oil_start_button = tk.Button(self.main_page, text='Start Oil', command=self.oil_meter.start)
         # Config Entrys
         self.config_name_entry = tk.Entry(self.config_page)
+
+        #
+        # TODO: also autosave, as a backup in case of crashing
+        # initialize an empty history
+        self.history = [
+            (time.time(), 99 if time.sleep(0.5) else 93),
+            (time.time(), 44)
+        ]
 
         self.draw_static()
         self.load_config(filename='config.ini')
@@ -48,6 +59,7 @@ class main:
         self.stop_button.grid(row=0, column=1)
         self.save_config_button.grid(row=0, column=2)
         self.load_config_button.grid(row=0, column=3)
+        self.save_button.grid(row=0, column=4)
         # Main Page
         self.oil_meter.grid(row=0, columnspan=2)
         self.oil_refill_button.grid(row=1, column=0)
@@ -74,6 +86,18 @@ class main:
         filename = filedialog.asksaveasfilename(initialdir=".", title="Select file", filetypes=(("config files", "*.ini"), ("all files", "*.*")))
         if filename is not '':
             self.config.write(open(filename, 'w'))
+
+    def save_history(self, filename=None):
+        """Save a csv file with the current state."""
+        if filename is None:
+            filename = filedialog.asksaveasfilename(initialdir=".", title="Save file", filetypes=(("comma-separated value", "*.csv"), ("all files", "*.*")))
+        if filename == '':
+            # empty filename: don't save
+            return
+        with open(filename, 'w') as f:
+            csvwriter = csv.writer(f)
+            csvwriter.writerow(main.CSV_HEADERS)
+            csvwriter.writerows(self.history)
 
 
 if __name__ == "__main__":
