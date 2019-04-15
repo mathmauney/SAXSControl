cd1ddbda0ff5729bd1e23157a2acf38c49142428
mathmauney <mathmauney@gmail.com>
2019-04-15 13:29:50 -0400

    Preliminary Layout

--- gui.py
+++ gui.py
@@ -4,7 +4,7 @@ import tkinter as tk
 from tkinter import filedialog
 from widgets import FluidLevel
 import tkinter.ttk as ttk
-from configparser import SafeConfigParser
+from configparser import ConfigParser
 
 
 class main:
@@ -14,23 +14,35 @@ class main:
         """Set up the window and button variables."""
         self.main_window = window
         self.main_window.title('Main Window')
-        # self.main_window.attributes("-fullscreen", True)  # Makes the window fullscreen
+        self.main_window.attributes("-fullscreen", True)  # Makes the window fullscreen
+        # Figure out geometry
+        window_width = self.main_window.winfo_screenwidth()
+        window_height = self.main_window.winfo_screenheight()
+        core_width = round(2*window_width/3)
+        log_width = window_width - core_width - 3
+        state_height = 300
+        core_height = window_height - state_height - 50
+        log_height = core_height
         # Button Bar
         self.buttons = tk.Frame(self.main_window)
-        self.exit_button = tk.Button(self.buttons, text='Exit', command=self.main_window.destroy)
-        self.stop_button = tk.Button(self.buttons, text='Stop', command=self.stop)
-        self.save_config_button = tk.Button(self.buttons, text='Save Config', command=self.save_config)
-        self.load_config_button = tk.Button(self.buttons, text='Load Config', command=self.load_config)
+        self.exit_button = tk.Button(self.main_window, text='X', command=self.main_window.destroy)
+        self.stop_button = tk.Button(self.buttons, text='STOP', command=self.stop, fg='red', font='Arial 16 bold')
         # Main Structures
-        self.core = ttk.Notebook(self.main_window)
-        self.main_page = tk.Frame(self.core)
+        self.core = ttk.Notebook(self.main_window, width=core_width, height=core_height)
+        self.auto_page = tk.Frame(self.core)
         self.config_page = tk.Frame(self.core)
+        self.manual_page = tk.Frame(self.core)
+        self.setup_page = tk.Frame(self.core)
+        self.log_frame = tk.Frame(self.main_window, width=log_width, height=log_height, bg='white')
+        self.state_frame = tk.Frame(self.main_window, width=window_width, height=state_height, bg='blue')
         # Widgets on Main page
         self.oil_ticksize = tk.IntVar(value=5)
-        self.oil_meter = FluidLevel(self.main_page, color='black', ticksize=self.oil_ticksize)
-        self.oil_refill_button = tk.Button(self.main_page, text='Refill Oil', command=lambda: self.oil_meter.update(100))
-        self.oil_start_button = tk.Button(self.main_page, text='Start Oil', command=self.oil_meter.start)
-        # Config Entrys
+        self.oil_meter = FluidLevel(self.auto_page, color='black', ticksize=self.oil_ticksize)
+        self.oil_refill_button = tk.Button(self.auto_page, text='Refill Oil', command=lambda: self.oil_meter.update(100))
+        self.oil_start_button = tk.Button(self.auto_page, text='Start Oil', command=self.oil_meter.start)
+        # Config page
+        self.save_config_button = tk.Button(self.config_page, text='Save Config', command=self.save_config)
+        self.load_config_button = tk.Button(self.config_page, text='Load Config', command=self.load_config)
         self.config_oil_tick_size_label = tk.Label(self.config_page, text='Oil Use (mL/min)')
         self.config_oil_tick_size = tk.Spinbox(self.config_page, from_=0, to=10, textvariable=self.oil_ticksize, increment=0.01)
 
@@ -39,23 +51,27 @@ class main:
 
     def draw_static(self):
         """Define the geometry of the frames and objects."""
-        self.buttons.grid(row=0, column=0)
+        self.buttons.grid(row=0, column=0, columnspan='2')
+        self.exit_button.grid(row=0, column=1, sticky='NE')
         self.core.grid(row=1, column=0)
+        self.log_frame.grid(row=1, column=1, sticky='S', pady=1)
+        self.state_frame.grid(row=2, column=0, columnspan=2)
         # Tab Bar
-        self.core.add(self.main_page, text='Main')
+        self.core.add(self.auto_page, text='Auto')
+        self.core.add(self.manual_page, text='Manual')
         self.core.add(self.config_page, text='Config')
+        self.core.add(self.setup_page, text='Setup')
         # Buttons
-        self.exit_button.grid(row=0, column=0)
-        self.stop_button.grid(row=0, column=1)
-        self.save_config_button.grid(row=0, column=2)
-        self.load_config_button.grid(row=0, column=3)
+        self.stop_button.grid(row=0, column=0)
         # Main Page
         self.oil_meter.grid(row=0, columnspan=2)
         self.oil_refill_button.grid(row=1, column=0)
         self.oil_start_button.grid(row=1, column=1)
         # Config page
-        self.config_oil_tick_size_label.grid(row=0, column=0)
-        self.config_oil_tick_size.grid(row=0, column=1)
+        self.save_config_button.grid(row=0, column=0)
+        self.load_config_button.grid(row=0, column=1)
+        self.config_oil_tick_size_label.grid(row=1, column=0)
+        self.config_oil_tick_size.grid(row=1, column=1)
 
     def stop(self):
         """Stop all running widgets."""
@@ -63,7 +79,7 @@ class main:
 
     def load_config(self, filename=None):
         """Load a config.ini file."""
-        self.config = SafeConfigParser()
+        self.config = ConfigParser()
         if filename is None:
             filename = filedialog.askopenfilename(initialdir=".", title="Select file", filetypes=(("config files", "*.ini"), ("all files", "*.*")))
         if filename is not '':
