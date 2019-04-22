691effe9820b0b1785d15990c928a3d677d6c802
mathmauney <mathmauney@gmail.com>
2019-04-22 12:32:17 -0400

    Add logging and gui for connecting to spec

--- SPEC.py
+++ SPEC.py
@@ -6,6 +6,7 @@ class connection(socket.socket):
     """Connection to a SPEC socket server and allows for sending commands"""
 
     def __init__(self, *args, **kwargs):
+        self.logger = kwargs.pop('logger')
         super(connection, self).__init__(socket.AF_INET, socket.SOCK_STREAM, **kwargs)
 
     def command(self, string):
@@ -14,3 +15,7 @@ class connection(socket.socket):
     def response(self, bytes=4096):
         response = self.recv(bytes).decode()
         return response
+
+    def connect(self, *args, **kwargs):
+        super(connection, self).connect(*args, **kwargs)
+        self.logger.append('Connected!')
--- widgets.py
+++ widgets.py
@@ -1,6 +1,8 @@
 """This module implements custom tkinter widgets for the SAXS control panel."""
 
 import tkinter as tk
+import logging
+import tkinter.scrolledtext as ScrolledText
 
 
 class FluidLevel(tk.Canvas):
@@ -51,3 +53,35 @@ class FluidLevel(tk.Canvas):
     def stop(self):
         """Turn off ticking behavior."""
         self.running = False
+
+
+class TextHandler(logging.Handler):
+    # This class allows you to log to a Tkinter Text or ScrolledText widget
+    # Adapted from Moshe Kaplan: https://gist.github.com/moshekaplan/c425f861de7bbf28ef06
+
+    def __init__(self, text):
+        # run the regular Handler __init__
+        logging.Handler.__init__(self)
+        # Store a reference to the Text it will log to
+        self.text = text
+
+    def emit(self, record):
+        msg = self.format(record)
+
+        def append():
+            self.text.configure(state='normal')
+            self.text.insert(tk.END, msg + '\n')
+            self.text.configure(state='disabled')
+            # Autoscroll to the bottom
+            self.text.yview(tk.END)
+        # This is necessary because we can't modify the Text from other threads
+        self.text.after(0, append)
+
+
+class MiscLogger(ScrolledText.ScrolledText):
+    def append(self, msg):
+        self.configure(state='normal')
+        self.insert(tk.END, msg + '\n')
+        self.configure(state='disabled')
+        # Autoscroll to the bottom
+        self.yview(tk.END)
